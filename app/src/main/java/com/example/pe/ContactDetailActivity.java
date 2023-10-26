d3274bd90dbec6fe444c58a1f7980ef92215bf02
ThangPQ <phamthangyn@gmail.com>
2023-10-26 17:29:32 +0700

    commit local

--- app/src/main/java/com/example/pe/ContactDetailActivity.java
+++ app/src/main/java/com/example/pe/ContactDetailActivity.java
@@ -2,7 +2,11 @@ package com.example.pe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.pe.entity.Contact;
+import com.example.pe.helpers.FirebaseDatabaseHelper;
 
 public class ContactDetailActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class ContactDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_detail);
+
+        String id = getIntent().getStringExtra("CONTACT_ID");
+        if(id==null || id.isEmpty()){
+            Intent intent = new Intent(ContactDetailActivity.this, MainActivity.class);
+            startActivity(intent);
+            return;
+        }
+        final FirebaseDatabaseHelper dbHelper = new FirebaseDatabaseHelper();
+        //Contact contact = dbHelper.getContact(id, null);
     }
 }
